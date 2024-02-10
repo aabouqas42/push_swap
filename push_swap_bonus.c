cf99e113218d8c76c859e298aa915269553dafc0
aabouqas <Aissam520@gmail.com>
2024-02-10 14:24:13 +0100

    done

--- push_swap_bonus.c
+++ push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aabouqas <aabouqas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:28:21 by aabouqas          #+#    #+#             */
-/*   Updated: 2024/02/10 14:07:40 by aabouqas         ###   ########.fr       */
+/*   Updated: 2024/02/10 14:24:08 by aabouqas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,16 +76,10 @@ void	_checker(t_data *data)
 	}
 }
 
-void lks()
-{
-	system("leaks checker");
-}
-
 int	main(int ac, char *av[])
 {
 	t_data	data;
 
-	atexit(lks);
 	mem_free(data.input);
 	data.stack_a = NULL;
 	data.input = get_stack(av);
